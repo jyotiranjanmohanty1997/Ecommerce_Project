ae0d05f08ab87d752631e7754cdec802a8d08e12
jyotiranjanmohanty1997 <jr507287@gmail.com>
2025-11-30 14:48:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import About from "./pages/About";
@@ -17,21 +17,23 @@ import TrackOrder from "./pages/TrackOrder";
 function App() {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <Navbar />
-      <SearchBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/product/:productId" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/placeorder" element={<PlaceOrder />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/trackorder" element={<TrackOrder/>}/>
-      </Routes>
-      <Footer />
+      <HashRouter>
+        <Navbar />
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/placeorder" element={<PlaceOrder />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/trackorder" element={<TrackOrder />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <StrictMode>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </HashRouter>
+  </StrictMode>
 );
